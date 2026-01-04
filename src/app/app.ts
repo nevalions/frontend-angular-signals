a82782f444d12356dba782b0a1670069d14cfb73
linroot <nevalions@gmail.com>
2026-01-04 13:41:35 +0300

    feat(ui): configure Taiga UI dark mode and add-on packages
    - Install @taiga-ui/addon-table and @taiga-ui/addon-mobile packages
    - Configure dark mode in app.ts using TUI_DARK_MODE provider
    - Update angular.json to include add-on-mobile styles in build config
    - Add add-on packages to allowedCommonJsDependencies
    
    Resolves STAF-122

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { TUI_DARK_MODE } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
+  providers: [
+    {
+      provide: TUI_DARK_MODE,
+      useValue: Object.assign(signal(true), { reset: () => {} }),
+    },
+  ],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
